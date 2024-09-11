7053c298eeacecb9750f4d7a3e8a0981c37ab367
Eric Graham <eric.graham@gmail.com>
2024-09-10 19:18:40 -0500

    starting Week 4, tidyr notes

---
+++ [04]/tidyr_notes.R
@@ -0,0 +1,27 @@
+library(tidyr)
+library(dplyr)
+who
+
+who1 = who %>% 
+  gather(
+    new_sp_m014:newrel_f65, key = "key",
+    value = "cases",
+    na.rm = TRUE
+  )
+who1
+
+who2 = who1 %>% 
+  mutate(key = stringr::str_replace(key, "newrel", "new_rel"))
+who2
+
+who3 = who2 %>% 
+  separate(key, c("new", "type", "sexage", sep = "_"))
+who3
+
+who4 = who3 %>% 
+  select(-new, -iso2, -iso3)
+
+who5 = who4 %>% 
+  separate(sexage, c("sex", "age"), sep = 1)
+who5
+# where did that underscore come from?
