578b077512881b437cdbd6f0758d74a2c8b48060
Eric Graham <eric.graham@gmail.com>
2024-09-11 21:04:51 -0500

    finishing lectures for Week 4

--- [04]/tidyr_notes.R
+++ [04]/tidyr_notes.R
@@ -1,5 +1,7 @@
 library(tidyr)
 library(dplyr)
+library(jsonlite)
+library(ggplot2)
 who
 
 who1 = who %>% 
@@ -25,3 +27,13 @@ who5 = who4 %>%
   separate(sexage, c("sex", "age"), sep = 1)
 who5
 # where did that underscore come from?
+
+# json
+test = jsonlite::fromJSON("[04]/64KB.json")
+head(test)
+
+test2 = jsonlite::fromJSON("[04]/1MB.json")
+head(test2)
+str(test2)
+test2 %>% ggplot(aes(x = language)) +
+  geom_histogram(stat = "count")
